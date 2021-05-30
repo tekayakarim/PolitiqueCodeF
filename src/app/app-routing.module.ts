1f5480be2fcc62d21a536f1bbcedf935b2027dec
tekayakarim <abdelkarimbentekaya@gmail.com>
2021-05-30 20:05:04 +0200

    intial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './components/shared/login/login.component';
+import { RegisterComponent } from './components/shared/register/register.component';
+import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
+import { NotFoundComponent } from './components/shared/not-found/not-found.component';
+
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'acceuil', component: NavBarComponent },
+  { path: '**', component: NotFoundComponent },
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { AcceuilComponent } from './components/acceuil/acceuil.component';
+
+import { LoginComponent } from './components/shared/login/login.component';
+import { RegisterComponent } from './components/shared/register/register.component';
+import { NavBarComponent } from './components/shared/nav-bar/nav-bar.component';
+import { NotFoundComponent } from './components/shared/not-found/not-found.component';
+
+import { AddArticleComponent } from './components/article/add-article/add-article.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcceuilComponent,
+ 
+    LoginComponent,
+    RegisterComponent,
+    NavBarComponent,
+    NotFoundComponent,
+    AddArticleComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
